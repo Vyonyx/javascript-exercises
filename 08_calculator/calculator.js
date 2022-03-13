4b77eb77adfaefd9855c257e373cb6c3ef750e13
Shaneel Kumar <shaneel_kumar@live.com>
2022-03-14 08:13:21 +1300

    Finished calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a ,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	const arraySum = inputArray.reduce(function(total, num) {return total + num}, 0);
+  return arraySum
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  const firstNumber = inputArray[0];
+  const newArray = inputArray.slice(1);
+  const multipleTotal = newArray.reduce(function(total, num) {
+    return total * num
+  }, firstNumber);
+  return multipleTotal
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 2) return 1
+  let startingNumber = num;
+  let factoralTotal = 0;
+  for (i = startingNumber; i > 0; i--) {
+    if (factoralTotal === 0) {
+      factoralTotal = startingNumber;
+    } else {
+      factoralTotal *= i;
+    }
+  };
+  return factoralTotal
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (inputWord) {
+    const word = inputWord.toLowerCase().replace(/,!'"/g, '');
+    const ln = word.length;
 
+    for (w in word) {
+        if (word[w] != word[(w + 1) * -1]) {return false}
+    };
+    return true
 };
 
 // Do not edit below this line
