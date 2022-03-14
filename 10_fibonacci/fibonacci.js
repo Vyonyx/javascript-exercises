143735b632868dd4bcffa7772f6224d2e728a5bd
Shaneel Kumar <shaneel_kumar@live.com>
2022-03-15 00:02:47 +1300

    Finished all the remaining exercises for fundamentals 5.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    
+    if (typeof num == 'string') {
+        num = parseInt(num);
+    }
+    if (num < 0) return 'OOPS'
 
+    let fibArray = [1];
+
+    for (i = 1; i < num; i++) {
+        if (i < 3) {
+            fibArray.push(i);
+        } else {
+            const numSum = fibArray[i - 1] + fibArray[i - 2]
+            fibArray.push(numSum);
+        }
+    }
+    return fibArray[fibArray.length -1];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,80 @@
-const findTheOldest = function() {
+// const findTheOldest = function(listOfPeople) {
 
+//     let oldestPerson = '';
+
+//     for (x in listOfPeople) {
+//         // If oldest person is empty, replace with current person.
+//         if (oldestPerson == '') {
+//             oldestPerson = listOfPeople[x];
+//         // Replace oldest person with current person IF they are living while the oldest is already dead.
+//         } else if (!listOfPeople[x]['yearOfDeath'] && oldestPerson['yearOfDeath']) {
+//             oldestPerson = listOfPeople[x];
+//         // Replace oldest person with current person IF both are living but current person is oldest.
+//         } else if (!listOfPeople[x]['yearOfDeath'] && !oldestPerson['yearOfDeath']) {
+//             let oldestPersonAge = new Date().getFullYear - oldestPerson['yearOfBirth'];
+//             let currentPersonAge = new Date().getFullYear - listOfPeople[x]['yearOfBirth'];
+//             if (currentPersonAge > oldestPersonAge) {
+//                 oldestPerson = listOfPeople[x];
+//             }
+//         // Replace oldest person with current IF both are death and the current is oldest.
+//         } else if (!oldestPerson['yearOfDeath'] && listOfPeople[x]['yearOfDeath']) {
+//             continue
+//         } else {
+//             let oldestPersonAge = oldestPerson['yearOfDeath'] - oldestPerson['yearOfBirth'];
+//             let currentPersonAge = listOfPeople[x]['yearOfDeath'] - listOfPeople[x]['yearOfBirth']
+//             if (currentPersonAge > oldestPersonAge) {
+//                 oldestPerson = listOfPeople[x];
+//             }
+//         }
+//     }
+//     return oldestPerson
+// };
+
+const findTheOldest = function(listOfPeople) {
+
+    let oldestPerson = '';
+
+    for (x in listOfPeople) {
+        // If oldest person is empty, replace with current person.
+        if (oldestPerson == '') {
+            oldestPerson = listOfPeople[x];
+        // Replace oldest person with current person IF both are living but current person is oldest.
+        } else {
+            today = new Date().getFullYear();
+
+            oldestBirth = oldestPerson['yearOfBirth'];
+            personBirth = listOfPeople[x]['yearOfBirth'];
+
+            oldestPersonAge = !oldestPerson['yearOfDeath'] ? today - oldestBirth : oldestPerson['yearOfDeath'] - oldestBirth;
+            personAge = !listOfPeople[x]['yearOfDeath'] ? today - personBirth : listOfPeople[x]['yearOfDeath'] - personBirth;
+
+            if (personAge > oldestPersonAge) {
+                oldestPerson = listOfPeople[x];
+                continue
+            }
+        }
+    }
+    return oldestPerson
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
